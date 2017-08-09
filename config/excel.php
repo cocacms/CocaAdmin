ffeb05e968dbf27a326128e151b833869f344bef
rojer Chen <rojerchen@qq.com>
2017-08-09 13:06:05 +0800

    adjust

--- config/excel.php
+++ config/excel.php
@@ -22,7 +22,7 @@ return array(
         | memory|gzip|serialized|igbinary|discISAM|apc|memcache|temp|wincache|sqlite|sqlite3
         |
         */
-        'driver'   => 'discISAM',
+        'driver'   => env('EXCEL_DRIVER', 'discISAM'),
 
         /*
         |--------------------------------------------------------------------------
