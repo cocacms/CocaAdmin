380fba07d1fbed66d0be42b5fcf75235e8a2dcbc
rojer Chen <rojerchen@qq.com>
2017-08-13 18:48:09 +0800

    adjust

--- resources/views/web/layout.blade.php
+++ resources/views/web/layout.blade.php
@@ -16,20 +16,20 @@
     <!-- No Baidu Siteapp-->
     <meta http-equiv="Cache-Control" content="no-siteapp"/>
 
-    <!-- <link rel="icon" type="image/png" href="assets/i/favicon.png"> -->
+    <link rel="icon" type="image/png" href="{{asset(system_config('weblogo'))}}">
 
     <!-- Add to homescreen for Chrome on Android -->
     <meta name="mobile-web-app-capable" content="yes">
-    <!-- <link rel="icon" sizes="192x192" href="assets/i/app-icon72x72@2x.png"> -->
+    <link rel="icon" sizes="192x192" href="{{asset(system_config('weblogo'))}}">
 
     <!-- Add to homescreen for Safari on iOS -->
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black">
     <meta name="apple-mobile-web-app-title" content="Amaze UI"/>
-    <!-- <link rel="apple-touch-icon-precomposed" href="assets/i/app-icon72x72@2x.png"> -->
+    <link rel="apple-touch-icon-precomposed" href="{{asset(system_config('weblogo'))}}">
 
     <!-- Tile icon for Win8 (144x144 + tile color) -->
-    <!-- <meta name="msapplication-TileImage" content="assets/i/app-icon72x72@2x.png"> -->
+    <meta name="msapplication-TileImage" content="{{asset(system_config('weblogo'))}}">
     <meta name="msapplication-TileColor" content="#0e90d2">
 
     <link rel="stylesheet" href="{{asset('web/css/amazeui.min.css')}}" media="all" />
